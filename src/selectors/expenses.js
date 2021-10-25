fec4d6e5702285d30d50fafa124ae6647d5d8429
DDbash <dbash112@hotmail.com>
2021-10-25 13:48:17 +0545

    project commit

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -6,26 +6,7 @@ const selectExpenses = (expenses, { text, categorizedBy, sortBy, startDate, endD
         const startDateMatch = startDate ? startDate.isSameOrBefore(createdAt, 'day') : true;
         const endDateMatch = endDate ? endDate.isSameOrAfter(createdAt, 'day') : true;
         const category = categorizedBy.toLowerCase() === 'all category' ? true : expense.category === categorizedBy;
-
-        let textMatch = expense.description;
-
-        //Naive Pattern Searching Algorithm
-        let count = 0;
-        if (text.length) {
-            textMatch = "";
-            for (let i = 0; i < expense.description.length; i++) {
-                for (let j = 0; j < text.length; j++) {
-                    if (j === expense.description.length) break;
-                    if (expense.description[i + j] === undefined) break;
-                    if (text[j].toLowerCase() !== expense.description[i + j].toLowerCase()) break;
-                    if (j === text.length - 1) count++;
-                }
-                if (count) {
-                    textMatch = expense.description;
-                    break;
-                }
-            }
-        }
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch && category;
     }).sort((i, j) => {
